3b4bcdbb35f665c0efcf976f94527b6dfd534740
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2018-02-09 18:16:41 +0700

    Add Viblo Atom plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0 - Beginning of develop
+* Added information in README.md
+* Created Atom Package template
---
+++ spec/viblo-atom-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel'
+
+import VibloAtomView from '../lib/viblo-atom-plugin-view'
+
+describe('VibloAtomView', () => {
+    it('has one valid test', () => {
+        expect('life').toBe('easy')
+    })
+})
